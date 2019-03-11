2a0c70d0468c7b702da0cc0f1accf4c04da25dc1
becario <becario@proyecto.cert>
2019-03-10 20:50:00 -0600

    Lineas de instalacion de drush y composer.

--- drupUpdate.sh
+++ drupUpdate.sh
@@ -19,12 +19,25 @@ else
 fi
 if isinstalled composer; then
 	echo `composer --version`
+	cgr drush/drush
 else
 	echo "No se encuentra instalado composer. Instalando"
-	
+	sudo yum install php-cli php-zip wget unzip
+	#Obteniendo composer
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	#Obteniendo hash de composer
+	HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+	#Verificando, si es real manda el mensaje "Instaler verified" y se puede continuar...
+	php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	#Si se verifico el hash, se instala el composer
+	sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+	#Instalando cgr
+	composer global require consolidation/cgr
+	#Agregando la direccion de cgr al path
+	PATH="$(composer config -g home)/vendor/bin:$PATH"
+	#Instalando drush
+	cgr drush/drush
 fi
-#d=isinstalled drupal
-#c=isinstalled composer
- 
+
 echo "Se procede a realizar el backup de /var/www. Esto puede tardar unos minutos".
 
